3af7bab31d4907d9464d6339371073cad08ff11d
aliba <19654755+alice-ba@users.noreply.github.com>
2018-05-21 23:07:47 +0200

    init

---
+++ src/recipe/Recipe.jsx
@@ -0,0 +1,37 @@
+import React, { PureComponent } from 'react';
+
+export default class Recipe extends PureComponent {
+    constructor (){
+        super();
+        this.state = {
+            recipes: [],
+        }
+    };
+
+
+componentDidMount(){
+    fetch('http://localhost:8080/recipes')
+    .then(results => {
+        return results.json();
+    }).then (data => {
+        let recipes = data.results.map((recipe,index) =>{
+            return(
+                <div key = {index}>
+                <p>{recipe.name}</p>
+                
+                </div>
+            )
+        })
+        this.setState({recipes});
+    })
+}
+render(){
+    return (
+<div> {this.state.recipes}</div>
+    )
+    
+}
+
+
+
+}
